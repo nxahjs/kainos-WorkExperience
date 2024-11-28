3af0a0b5b24145633a50b969ee5ea79a5c9ab7bc
Noah Smyth <noah@noahsmyth.xyz>
2024-11-28 15:00:15 +0000

    Upload site files

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+function signUp() {
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var email = document.getElementById("email").value;
+    
+    alert("Successfully signed up " + email + " for weekly updates!")
+}
